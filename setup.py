fdfd03422cde91f8bdd806f47167b4253e30cc52
Roman Isecke <136338424+rbiseck3@users.noreply.github.com>
2024-07-30 15:38:24 -0400

    feat: Add code to publish to pypi (#16)

--- setup.py
+++ setup.py
@@ -1,7 +1,29 @@
+"""
+setup.py
+
+unstructured-platform-plugins - utility library for unstructured plugin development
+
+Copyright 2022 Unstructured Technologies, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from pathlib import Path
 from typing import List, Union
 
 from setuptools import find_packages, setup
+
+from unstructured_platform_plugins.__version__ import __version__
 
 
 def load_requirements(file: Union[str, Path]) -> List[str]:
@@ -26,10 +48,28 @@ def load_requirements(file: Union[str, Path]) -> List[str]:
 
 setup(
     name="unstructured-platform-plugins",
-    version="1.0.0",
+    version=__version__,
+    description="A utility library that provides tools around unstructured plugin development",
+    long_description=open("README.md", encoding="utf-8").read(),  # noqa: SIM115
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
     python_requires=">=3.10,<3.13",
     url="https://github.com/Unstructured-IO/unstructured-platform-plugins",  # noqa: 501
     packages=find_packages(),
+    license="Apache-2.0",
     entry_points={
         "console_scripts": [
             "etl-uvicorn=unstructured_platform_plugins.etl_uvicorn.main:cmd",
