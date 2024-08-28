82b900c77f398026bcbc1c6521af130b17941c3e
Roman Isecke <roman@unstructured.io>
2024-08-28 09:46:29 -0400

    Add otel metrics support as well

--- unstructured_platform_plugins/etl_uvicorn/api_generator.py
+++ unstructured_platform_plugins/etl_uvicorn/api_generator.py
@@ -11,7 +11,7 @@ from pydantic import BaseModel
 from starlette.responses import RedirectResponse
 from uvicorn.importer import import_from_string
 
-from unstructured_platform_plugins.etl_uvicorn.otel import get_provider
+from unstructured_platform_plugins.etl_uvicorn.otel import get_metric_provider, get_trace_provider
 from unstructured_platform_plugins.etl_uvicorn.utils import (
     get_func,
     get_input_schema,
@@ -172,6 +172,8 @@ def generate_fast_api(
     except TypeError as e:
         raise TypeError(f"failed to validate function schema: {e}") from e
 
-    FastAPIInstrumentor.instrument_app(fastapi_app, tracer_provider=get_provider())
+    FastAPIInstrumentor.instrument_app(
+        fastapi_app, tracer_provider=get_trace_provider(), meter_provider=get_metric_provider()
+    )
 
     return fastapi_app
--- unstructured_platform_plugins/etl_uvicorn/otel.py
+++ unstructured_platform_plugins/etl_uvicorn/otel.py
@@ -1,8 +1,14 @@
 import os
 from typing import Literal, TypedDict
 
-from opentelemetry.environment_variables import OTEL_TRACES_EXPORTER
+from opentelemetry.environment_variables import OTEL_METRICS_EXPORTER, OTEL_TRACES_EXPORTER
 from opentelemetry.sdk.environment_variables import OTEL_SERVICE_NAME
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import (
+    ConsoleMetricExporter,
+    MetricReader,
+    PeriodicExportingMetricReader,
+)
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import (
@@ -10,32 +16,52 @@ from opentelemetry.sdk.trace.export import (
     SimpleSpanProcessor,
 )
 
-ExporterType = Literal["otlp", "jaeger", "zipkin", "console"]
+TraceExporterType = Literal["otlp", "jaeger", "zipkin", "console"]
+MetricExporterType = Literal["otlp", "prometheus", "none"]
 
 
 class OtelSettings(TypedDict):
     service_name: str
-    exporters: list[ExporterType]
+    trace_exporters: list[TraceExporterType]
+    metric_exporters: list[MetricExporterType]
 
 
 def get_settings() -> OtelSettings:
     service_name = os.environ.get(OTEL_SERVICE_NAME, "unknown_service")
-    exporters = os.environ.get(OTEL_TRACES_EXPORTER)
-    exporters = exporters.split(",") if exporters else []
-    return OtelSettings(service_name=service_name, exporters=exporters)
+    trace_exporters = os.environ.get(OTEL_TRACES_EXPORTER)
+    trace_exporters = trace_exporters.split(",") if trace_exporters else []
+
+    metric_exporters = os.environ.get(OTEL_METRICS_EXPORTER)
+    metric_exporters = metric_exporters.split(",") if metric_exporters else []
+    return OtelSettings(
+        service_name=service_name,
+        trace_exporters=trace_exporters,
+        metric_exporters=metric_exporters,
+    )
 
 
-def get_provider() -> TracerProvider:
+def get_trace_provider() -> TracerProvider:
     settings = get_settings()
+    print(settings)
     provider = TracerProvider(resource=Resource({SERVICE_NAME: settings["service_name"]}))
 
-    for exporter_type in settings["exporters"]:
-        _add_exporter(exporter_type=exporter_type, provider=provider)
+    for trace_exporter_type in settings["trace_exporters"]:
+        _add_trace_exporter(exporter_type=trace_exporter_type, provider=provider)
 
     return provider
 
 
-def _add_exporter(exporter_type: ExporterType, provider: TracerProvider):
+def get_metric_provider() -> MeterProvider:
+    settings = get_settings()
+    readers = []
+    for metric_exporter_type in settings["metric_exporters"]:
+        readers.append(_get_metrics_reader(exporter_type=metric_exporter_type))
+    return MeterProvider(
+        resource=Resource({SERVICE_NAME: settings["service_name"]}), metric_readers=readers
+    )
+
+
+def _add_trace_exporter(exporter_type: TraceExporterType, provider: TracerProvider):
     if exporter_type == "otlp":
         _add_traces_otlp_exporter(
             provider,
@@ -45,6 +71,14 @@ def _add_exporter(exporter_type: ExporterType, provider: TracerProvider):
         _add_traces_console_exporter(provider)
     else:
         raise NotImplementedError(f"{exporter_type} implementation not supported yet")
+
+
+def _get_metrics_reader(exporter_type: MetricExporterType) -> MetricReader:
+    if exporter_type == "otlp":
+        return _get_metric_otlp_reader()
+    if exporter_type == "console":
+        return _get_metric_console_reader()
+    raise NotImplementedError(f"{exporter_type} implementation not supported yet")
 
 
 def _add_traces_console_exporter(provider: TracerProvider) -> None:
@@ -59,3 +93,15 @@ def _add_traces_otlp_exporter(provider: TracerProvider) -> None:
     exporter = OTLPSpanExporter()
     processor = SimpleSpanProcessor(exporter)
     provider.add_span_processor(processor)
+
+
+def _get_metric_otlp_reader() -> MetricReader:
+    from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+
+    exporter = OTLPMetricExporter()
+    return PeriodicExportingMetricReader(exporter)
+
+
+def _get_metric_console_reader() -> MetricReader:
+    exporter = ConsoleMetricExporter()
+    return PeriodicExportingMetricReader(exporter)
