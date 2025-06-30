761896247007cd534db0cc9397fab030022a3398
Filip Knefel <filip@unstructured.io>
2025-06-30 16:29:28 +0200

    Drop custom errors for HTTP exceptions

--- unstructured_platform_plugins/etl_uvicorn/errors.py
+++ unstructured_platform_plugins/etl_uvicorn/errors.py
@@ -20,10 +20,6 @@ class UserAuthError(UserError):
     status_code: int = 403
 
 
-class UnprocessableEntityError(UserError):
-    status_code: int = 422
-
-
 class RateLimitError(UserError):
     status_code: int = 429
 
@@ -45,16 +41,10 @@ class CatchAllError(BaseError):
 
 
 def wrap_error(e: Exception) -> HTTPException:
-    if isinstance(e, ingest_errors.UserAuthError):
-        return UserAuthError(e)
-    elif isinstance(e, HTTPException):
-        if e.status_code == 400:
-            return UserError(e)
-        if e.status_code == 422:
-            return UnprocessableEntityError(e)
-        if e.status_code == 504:
-            return GatewayTimeoutError(e)
+    if isinstance(e, HTTPException):
         return e
+    elif isinstance(e, ingest_errors.UserAuthError):
+        return UserAuthError(e)
     elif isinstance(e, ingest_errors.RateLimitError):
         return RateLimitError(e)
     elif isinstance(e, ingest_errors.QuotaError):
