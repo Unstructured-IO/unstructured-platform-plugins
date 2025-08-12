b794b72ddfdbc3441edf739447f4faf9be74c921
Bryan Chen <bryan.chen@unstructured.io>
2025-08-12 11:05:47 -0400

    remove unnecessary wrap error logic

--- unstructured_platform_plugins/etl_uvicorn/api_generator.py
+++ unstructured_platform_plugins/etl_uvicorn/api_generator.py
@@ -15,7 +15,6 @@ from unstructured_ingest.data_types.file_data import BatchFileData, FileData, fi
 from uvicorn.config import LOG_LEVELS
 from uvicorn.importer import import_from_string
 
-from unstructured_platform_plugins.etl_uvicorn.errors import wrap_error
 from unstructured_platform_plugins.etl_uvicorn.otel import get_metric_provider, get_trace_provider
 from unstructured_platform_plugins.etl_uvicorn.utils import (
     get_func,
@@ -185,7 +184,6 @@ def _wrap_in_fastapi(
                             )
                     except Exception as e:
                         logger.error(f"Failure streaming response: {e}", exc_info=True)
-                        http_error = wrap_error(e)
                         yield (
                             InvokeResponse(
                                 usage=usage,
@@ -193,7 +191,9 @@ def _wrap_in_fastapi(
                                 filedata_meta=filedata_meta_model.model_validate(
                                     filedata_meta.model_dump()
                                 ),
-                                status_code=http_error.status_code,
+                                status_code=e.status_code
+                                if hasattr(e, "status_code")
+                                else status.HTTP_500_INTERNAL_SERVER_ERROR,
                                 status_code_text=f"[{e.__class__.__name__}] {e}",
                             ).model_dump_json()
                             + "\n"
@@ -226,12 +226,13 @@ def _wrap_in_fastapi(
             )
         except Exception as invoke_error:
             logger.error(f"failed to invoke plugin: {invoke_error}", exc_info=True)
-            http_error = wrap_error(invoke_error)
             return InvokeResponse(
                 usage=usage,
                 message_channels=message_channels,
                 filedata_meta=filedata_meta_model.model_validate(filedata_meta.model_dump()),
-                status_code=http_error.status_code,
+                status_code=invoke_error.status_code
+                if hasattr(invoke_error, "status_code")
+                else status.HTTP_500_INTERNAL_SERVER_ERROR,
                 status_code_text=f"[{invoke_error.__class__.__name__}] {invoke_error}",
                 file_data=request_dict.get("file_data", None),
             )
